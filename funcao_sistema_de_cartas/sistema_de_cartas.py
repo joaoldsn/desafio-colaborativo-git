ce20ee26a0e7f31587f5a9a08c4135a9574cdb2a
vinicius-oliveira27 <vini.voaaraujoooo@gmail.com>
2026-09-15 10:51:45 -0300

    feat: adição de comentários

--- funcao_sistema_de_cartas/sistema_de_cartas.py
+++ funcao_sistema_de_cartas/sistema_de_cartas.py
@@ -1,5 +1,5 @@
-class Carta:
-    def __init__(self, nome, ataque, defesa):
-        self.nome = nome
+class Carta: # Cria uma classe chamada Carta. A classe funciona como um "molde" para criar cartas.
+    def __init__(self, nome, ataque, defesa): #O __init__ é o método chamado automaticamente quando criamos uma nova Carta.
+        self.nome = nome   # self = representa a própria carta que está sendo criada
         self.ataque = ataque
         self.defesa = defesa
